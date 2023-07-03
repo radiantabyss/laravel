0d9f6f9ece8548f7414b291c5dc33d0984f99c56
Robert Buchi <robert@planetweb.ro>
2023-07-04 01:01:34 +0300

    filter update

--- src/Filter.php
+++ src/Filter.php
@@ -3,15 +3,17 @@ namespace RA\Core;
 
 trait Filter
 {
+    protected static $table;
     protected static $query;
-    protected static $has_default_order;
 
-    public static function apply($query, $filters = null, $has_default_order = true) {
+    public static function apply($query, $filters = null) {
+        //set table
+        if ( !self::$table ) {
+            self::$table = \Str::snake(\Domain::get());
+        }
+
         //set query
         self::$query = $query;
-
-        //set default order
-        self::$has_default_order = $has_default_order;
 
         //get filters from request
         if ( $filters === null ) {
@@ -19,72 +21,83 @@ trait Filter
         }
 
         $operators = [];
-        foreach ($filters as $key => $value) {
-            if ( ($value === '' || $value === null) && !in_array($operators[$key] ?? '', ['is_empty', 'is_not_empty']) ) {
+        foreach ( $filters as $key => $value ) {
+            if ( !preg_match('/__operator/', $key) ) {
                 continue;
             }
 
-            if (in_array($key, ['order', 'order_by', 'page', 'per_page', 'export_csv'])) {
-                continue;
-            }
-
-            $operatorsCheck = explode('__operator', $key);
-            if ( count($operatorsCheck) == 2 ) {
-                $operators[$operatorsCheck[0]] = $value;
-                continue;
-            }
-
-            $operator = $operators[$key] ?? '=';
-
-            if (method_exists(__CLASS__, $key)) {
-                self::$key($value, $operator);
-            }
-            else {
-                if ( in_array($operator, ['in', 'not_in']) ) {
-                    preg_match_all('/(".*?",)|(".*?"$)/', $value, $matches);
-                    foreach ($matches[0] as &$field) {
-                        $field = ltrim(rtrim(str_replace('",', '', trim($field)),'"'), '"');
-                    }
-                    $value = $matches[0];
-                }
-
-                if ( $operator == 'is_empty' ) {
-                    self::$query->where(function ($q) use ($key) {
-                        $q->where($key, '=', '')
-                            ->orWhereNull($key);
-                    });
-                }
-                else if ( $operator == 'is_not_empty' ) {
-                    self::$query->where(function ($q) use ($key) {
-                        $q->whereNotNull($key)
-                            ->where($key, '<>', '');
-                    });
-                }
-                else if ( $operator == '!=' ) {
-                    self::$query->where($key, '<>', $value);
-                }
-                else if ( in_array($operator, ['=', '>', '<']) ) {
-                    self::$query->where($key, $operator, $value);
-                }
-                else if ( $operator == 'in' ) {
-                    self::$query->whereIn($key, $value);
-                }
-                else if ( $operator == 'not_in' ) {
-                    self::$query->whereNotIn($key, $value);
-                }
-                else if ( $operator == 'contains' ) {
-                    self::$query->where($key, 'LIKE', '%' . $value . '%');
-                }
-                else if ( $operator == 'not_contains' ) {
-                    self::$query->where($key, 'NOT LIKE', '%' . $value . '%');
-                }
-                else {
-                    self::$query->where($key, $value);
-                }
-            }
+            $operators[str_replace('__operator', '', $key)] = $value;
         }
 
+        //set order
         self::order_by($filters['order_by'] ?? '', $filters['order'] ?? 'asc');
+
+        foreach ($filters as $key => $value) {
+            //ignore empty values
+            if ( ($value === '' || $value === null) ) {
+                continue;
+            }
+
+            //ignore reserved keys
+            if ( in_array($key, ['order', 'order_by', 'page', 'per_page']) ) {
+                continue;
+            }
+
+            //ignore operators
+            if ( preg_match('/__operator/', $key) ) {
+                continue;
+            }
+
+            //check if a custom method is defined
+            if ( method_exists(__CLASS__, $key) ) {
+                self::$key($value, $operator);
+                continue;
+            }
+
+            $column = self::$table.'.'.$key;
+
+            //apply default where if no operator is specified
+            if ( !isset($operators[$key]) ) {
+                self::$query->where($column, $value);
+                continue;
+            }
+
+            //apply where based on the operator
+            $operator = $operators[$key] ?? '';
+
+            if ( $operator == 'is_empty' ) {
+                self::$query->where(function($query) use($column) {
+                    $query->where($column, '=', '')
+                        ->orWhereNull($column);
+                });
+            }
+            else if ( $operator == 'is_not_empty' ) {
+                self::$query->where(function($query) use($column) {
+                    $query->whereNotNull($column)
+                        ->where($column, '<>', '');
+                });
+            }
+            else if ( $operator == '!=' ) {
+                self::$query->where($column, '<>', $value);
+            }
+            else if ( in_array($operator, ['>', '<']) ) {
+                self::$query->where($column, $operator, $value);
+            }
+            else if ( $operator == 'in' ) {
+                $value = explode('|', $value);
+                self::$query->whereIn($column, $value);
+            }
+            else if ( $operator == 'not_in' ) {
+                $value = explode('|', $value);
+                self::$query->whereNotIn($column, $value);
+            }
+            else if ( $operator == 'contains' ) {
+                self::$query->where($column, 'LIKE', '%' . $value . '%');
+            }
+            else if ( $operator == 'not_contains' ) {
+                self::$query->where($column, 'NOT LIKE', '%' . $value . '%');
+            }
+        }
     }
 
     protected static function order_by($order_by, $order) {
@@ -96,28 +109,28 @@ trait Filter
                 self::$query->orderBy(trim($order_by), trim($orders[$i] ?? $orders[0]));
             }
         }
-        else if ( self::$has_default_order ) {
+        else {
             self::$query->orderBy('id', 'desc');
         }
     }
 
     protected static function name($value) {
-        self::$query->where('name', 'LIKE', '%' . $value . '%');
+        self::$query->where(self::$table.'.name', 'LIKE', '%' . $value . '%');
     }
 
     protected static function start_date($value) {
-        self::$query->where('date', '>=', $value);
+        self::$query->where(self::$table.'.date', '>=', $value);
     }
 
     protected static function end_date($value) {
-        self::$query->where('date', '<=', $value);
+        self::$query->where(self::$table.'.date', '<=', $value);
     }
 
     protected static function start_created_at($value) {
-        self::$query->where('created_at', '>=', apply_reverse_timezone($value, auth()->user()->timezone));
+        self::$query->where(self::$table.'.created_at', '>=', apply_reverse_timezone($value, auth()->user()->timezone));
     }
 
     protected static function end_created_at($value) {
-        self::$query->where('created_at', '<=', apply_reverse_timezone($value . ' 23:59:59', auth()->user()->timezone));
+        self::$query->where(self::$table.'.created_at', '<=', apply_reverse_timezone($value . ' 23:59:59', auth()->user()->timezone));
     }
 }
