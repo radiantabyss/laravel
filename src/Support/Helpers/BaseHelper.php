25368e40ed9e3d7048242e7c86db9710e1ae9547
Robert B <robert@planetweb.ro>
2024-06-13 11:30:14 +0300

    command exists null fix

--- src/Support/Helpers/BaseHelper.php
+++ src/Support/Helpers/BaseHelper.php
@@ -370,6 +370,11 @@ if ( !function_exists('command_exists') ) {
     function command_exists($command) {
         $is_windows = strpos(PHP_OS, 'WIN') === 0;
         $response = shell_exec(($is_windows ? 'where ' : 'which ').$command);
+        
+        if ( $response === null ) {
+            return false;
+        }
+
         if ( $is_windows && preg_match('/Could not find files for the given pattern/', $response) ) {
             return false;
         }
