89a9efcda9c5b6ddb0231b44b6237ac9fe5c6f50
Robert Buchi <robert@planetweb.ro>
2024-07-25 18:24:01 +0300

    restart jobs process

--- src/CoreServiceProvider.php
+++ src/CoreServiceProvider.php
@@ -14,10 +14,24 @@ class CoreServiceProvider extends ServiceProvider
         $this->enablePublishing();
     }
 
+    /**
+     * Register the application services.
+     */
+    public function register()
+    {
+        //load configs
+        $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'lumi-core');
+    }
+
     private function enablePublishing() {
         if ( !\App::runningInConsole() ) {
             return;
         }
+
+        //config
+        $this->publishes([
+            __DIR__.'/../config/config.php' => config_path('lumi-core.php'),
+        ], 'lumi-core:config');
 
         //migrations
         $this->publishes([
--- src/Job.php
+++ src/Job.php
@@ -16,4 +16,23 @@ class Job implements ShouldQueue
     public function __construct($params) {
         $this->params = $params;
     }
+
+    public function handle() {
+        if ( config('lumi-core.enable_job_process_restart') && !isset($GLOBALS['__jobs_count']) ) {
+            $GLOBALS['__jobs_count'] = 0;
+        }
+
+        $this->run();
+
+        //restart process after 30 jobs
+        if ( config('lumi-core.enable_job_process_restart') ) {
+            $GLOBALS['__jobs_count']++;
+            $max_jobs_count = config('lumi-core.max_jobs_per_process:'.$this->getQueue()) ?? config('lumi-core.max_jobs_per_process');
+
+            if ( $GLOBALS['__jobs_count'] == $max_jobs_count ) {
+                \DB::table('jobs')->where('id', $this->getJobId())->delete();
+                die();
+            }
+        }
+    }
 }
